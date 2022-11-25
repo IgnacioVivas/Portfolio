e7a9c701f9fbd8d840142f55c5edbb56d6e94c2d
IgnacioVivas <ignacab@hotmail.com>
2022-11-25 00:04:40 -0300

    agregando validaciones al form de contacto

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,22 +1,87 @@
+import { useState } from 'react';
 import './form.scss';
 
 function Form() {
+  const [inputValue, setInputValue] = useState({
+    name: '',
+    email: '',
+    texto: '',
+  });
+  const [errors, setErrors] = useState({});
+
+  const validate = (inputValue) => {
+    let errors = {};
+    if (!inputValue.name) errors.name = 'Este campo no puede estar vacío';
+    if (!inputValue.email) errors.email = 'Este campo no puede estar vacío';
+    if (!inputValue.texto) errors.texto = 'Este campo no puede estar vacío';
+    return errors;
+  };
+
+  const resetForm = (inputValue) => {
+    setInputValue({
+      ...inputValue,
+      name: '',
+      temperament: '',
+      weightImperial: '',
+      weightMetric: '',
+      heightImperial: '',
+      heightMetric: '',
+      lifeExpectancy: '',
+      origin: '',
+    });
+  };
+
+  const handleChange = (e) => {
+    setInputValue({ ...inputValue, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formValues = {
+      ...inputValue,
+      [e.target.name]: e.target.value,
+    };
+    setErrors(validate(formValues));
+
+    if (Object.keys(validate(formValues)).length === 0) {
+      resetForm();
+    }
+  };
   return (
-    <form id='container-form'>
+    <form id='container-form' onSubmit={handleSubmit}>
       <div>
-        <input type='text' placeholder='Tu nombre completo aquí' />
+        <input
+          className={errors.name && 'danger'}
+          type='text'
+          placeholder='Tu nombre completo aquí'
+          onChange={handleChange}
+          name='name'
+          value={inputValue.name}
+        />
+        {errors.name && <p className='danger'>{errors.name}</p>}
       </div>
       <div>
-        <input type='text' placeholder='Su dirección de correo electrónico' />
+        <input
+          className={errors.email && 'danger'}
+          type='text'
+          placeholder='Su dirección de correo electrónico'
+          onChange={handleChange}
+          name='email'
+          value={inputValue.email}
+        />
+        {errors.email && <p className='danger'>{errors.email}</p>}
       </div>
       <div>
         <textarea
-          name=''
+          className={errors.texto && 'danger'}
           id=''
           cols='30'
           rows='10'
           placeholder='Escriba su mensaje aquí...'
+          onChange={handleChange}
+          name='texto'
+          value={inputValue.texto}
         ></textarea>
+        {errors.texto && <p className='danger'>{errors.texto}</p>}
       </div>
       <button>Enviar mensaje</button>
     </form>
